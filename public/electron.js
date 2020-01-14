3f8be1bb3d17d72c5596fbf614fb8a9519e0deee
josh.castillo <josh@seer-technologies.com>
2020-01-14 15:25:32 +0800

    Fix electron.js menu

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,4 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, Menu, protocol, ipcMain } = require('electron');
 const {autoUpdater} = require("electron-updater");
 const path = require("path");
 const isDev = require('electron-is-dev')
