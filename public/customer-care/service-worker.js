6551c9d5c74b400d00cfba28b7bffbbb8960777a
Jae Lagonsin <jlagonsin@Jaes-MacBook-Pro.local>
2020-02-05 00:46:24 +0800

    fix cc whitescreen issue, fix devtools

--- public/customer-care/service-worker.js
+++ public/customer-care/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35c54145b43dbc075691eaf13cfeefae.js"
+  "./precache-manifest.1ab75bd091c3ca571bad18e8837d30fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
