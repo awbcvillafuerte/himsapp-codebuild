78885f7759270f08216887dfc104234f23476f6d
dev-doods <randell@seer-technologies.com>
2020-02-11 23:13:16 +0800

    0.2.9-alpha

---
+++ public/underwriting/static/js/20.3e5982b2.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpunderwriting=window.webpackJsonpunderwriting||[]).push([[20],{928:function(n,w){}}]);
+//# sourceMappingURL=20.3e5982b2.chunk.js.map
--- public/underwriting/static/js/20.5ca7bf03.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonpunderwriting=window.webpackJsonpunderwriting||[]).push([[20],{927:function(n,w){}}]);
-//# sourceMappingURL=20.5ca7bf03.chunk.js.map
