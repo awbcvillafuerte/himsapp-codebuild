04a4f57b4e5c05a5976ea2a9f455fe9b4098caed
dev-doods <randell@seer-technologies.com>
2020-02-19 19:45:02 +0800

    0.2.15-beta

---
+++ public/customer-care/precache-manifest.31b30705935566d5d01d64b0934bf7e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d3f538ad0498d744f613c2a9664668",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ecf0acedb39e5d0b5f5c",
+    "url": "./static/css/3.306bd6cf.chunk.css"
+  },
+  {
+    "revision": "c62d493a7d14831caeca",
+    "url": "./static/css/4.f0a752f0.chunk.css"
+  },
+  {
+    "revision": "b4d934f5557b1c643e21",
+    "url": "./static/css/5.1ad57a0d.chunk.css"
+  },
+  {
+    "revision": "38eeb02585b10386c956",
+    "url": "./static/css/main.0cac5455.chunk.css"
+  },
+  {
+    "revision": "0a05990fb5943874e8d6",
+    "url": "./static/js/2.446f4a1b.chunk.js"
+  },
+  {
+    "revision": "ecf0acedb39e5d0b5f5c",
+    "url": "./static/js/3.42f7194b.chunk.js"
+  },
+  {
+    "revision": "c62d493a7d14831caeca",
+    "url": "./static/js/4.7e4b568c.chunk.js"
+  },
+  {
+    "revision": "b4d934f5557b1c643e21",
+    "url": "./static/js/5.e709cdbd.chunk.js"
+  },
+  {
+    "revision": "e13e76199ebe53b45c47",
+    "url": "./static/js/6.63196c69.chunk.js"
+  },
+  {
+    "revision": "39c28f1a056a7d082423",
+    "url": "./static/js/7.7390739d.chunk.js"
+  },
+  {
+    "revision": "3a9d337e1f8aa437a1bc",
+    "url": "./static/js/8.8e769327.chunk.js"
+  },
+  {
+    "revision": "a09530e0eb86108aa276",
+    "url": "./static/js/9.3ac6fcef.chunk.js"
+  },
+  {
+    "revision": "38eeb02585b10386c956",
+    "url": "./static/js/main.1845c460.chunk.js"
+  },
+  {
+    "revision": "aa5dee2de1350f830c17",
+    "url": "./static/js/runtime~main.99e6d941.js"
+  }
+]);
