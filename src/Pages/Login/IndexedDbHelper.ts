991501ac723e5d268cbbd99153cb9017894c3a5c
dev-tjm <tj@seer-technologies.com>
2020-02-27 12:36:42 +0800

    implement reinitlization of stores, drop and create store if version changes

--- src/Pages/Login/IndexedDbHelper.ts
+++ src/Pages/Login/IndexedDbHelper.ts
@@ -3,6 +3,8 @@ export interface IndexedDBHelperResponse {
     result: any
 }
 
+const db_version = 4;
+
 export default class IndexedDbHelper {
 
     deleteDb = (namespace: string) => {
@@ -22,7 +24,7 @@ export default class IndexedDbHelper {
     }
 
     openDb = (namespace: string) => {
-        let request = window.indexedDB.open(namespace, 3);
+        let request = window.indexedDB.open(namespace, db_version);
 
         return new Promise<IndexedDBHelperResponse> ((resolve, reject) => {
 
@@ -57,30 +59,23 @@ export default class IndexedDbHelper {
     createStoreOnDb = (db: IDBDatabase, storeName: string, keyPath: string, indexes?: any[]) => {
         let store = db.createObjectStore(storeName, {keyPath, autoIncrement: false});
 
-        return new Promise<IndexedDBHelperResponse> ((resolve, reject) => {
-            let response: IndexedDBHelperResponse = {
-                cbType: '',
-                result: {}
-            };
+        if (indexes && indexes.length > 0) {
+            indexes.forEach((index: any) => {
+                store.createIndex(index.name, index.keyPath, { unique: index.unique });
+            })
+        }
 
-            if (indexes && indexes.length > 0) {
-                indexes.forEach((index: any) => {
-                    store.createIndex(index.name, index.keyPath, { unique: index.unique });
-                })
-            }
+        store.transaction.onerror = () => {
+            console.log("error creating store object.");
+        }
 
-            store.transaction.onerror = () => {
-                reject("error creating store on db.")
-            }
+        store.transaction.oncomplete = (event: any) => {                
+            console.log("store object successfully initialized.");
+        }
+    }
 
-            store.transaction.oncomplete = (event: any) => {
-                let result = event.target.result;
-                
-                response.cbType = event.type;
-                response.result = result;
-                resolve(response);
-            }
-        })
+    deleteObjectStore = (db: IDBDatabase, storeName: string) => {
+        db.deleteObjectStore(storeName);
     }
 
     saveToStore = (db: IDBDatabase, storeName: string, entries: any) => {
@@ -299,6 +294,7 @@ export default class IndexedDbHelper {
         })
     }
 }
+
 
 
 
