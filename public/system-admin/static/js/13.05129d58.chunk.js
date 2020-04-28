ff3754a4d423080f613ed27142d485e21b864a10
josh.castillo <josh@seer-technologies.com>
2020-04-28 12:56:04 +0800

    Build for Version 0.2.60-beta

--- public/system-admin/static/js/13.05129d58.chunk.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpsystem-admin"]=window["webpackJsonpsystem-admin"]||[]).push([[13],{810:function(n,s){}}]);
-//# sourceMappingURL=13.05129d58.chunk.js.map
---
+++ public/system-admin/static/js/13.c24585cd.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonpsystem-admin"]=window["webpackJsonpsystem-admin"]||[]).push([[13],{809:function(n,s){}}]);
+//# sourceMappingURL=13.c24585cd.chunk.js.map
