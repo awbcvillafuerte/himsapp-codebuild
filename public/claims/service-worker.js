c8f1ea4958e682e157974e80bca7d487542d79fc
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2020-10-05 18:44:20 +0800

    AWB Claims Sprint 30.20201005

--- public/claims/service-worker.js
+++ public/claims/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/claims/precache-manifest.5af699102a824cb4789572738f29ea3b.js"
+  "/claims/precache-manifest.f71b6d276c980be4f15d702180f7cf32.js"
 );
 
 self.addEventListener('message', (event) => {
