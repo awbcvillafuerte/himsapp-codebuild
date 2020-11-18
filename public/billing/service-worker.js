a2a830df3ecb6106a8cdb4fbac3be0069b96a18f
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2020-11-18 20:17:26 +0800

    BICO Sprint 19.2 beta Build

--- public/billing/service-worker.js
+++ public/billing/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/billing/precache-manifest.5622dac2fc043498417ed48a6805e667.js"
+  "/billing/precache-manifest.3487a1ca14b6723cb171ed29170ce128.js"
 );
 
 self.addEventListener('message', (event) => {
