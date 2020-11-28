175abec3e81ea305be25e60f5bbd0055140f44ce
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2020-11-28 10:42:37 +0800

    PARTNER-b.1.0.0-build.0.4.7.5-20201128

--- public/partner_network/service-worker.js
+++ public/partner_network/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/partner_network/precache-manifest.7702a109ea4635fe163cbd62f9363453.js"
+  "/partner_network/precache-manifest.c2b5dbae0c5b9b5a60441c553cecbce8.js"
 );
 
 self.addEventListener('message', (event) => {
