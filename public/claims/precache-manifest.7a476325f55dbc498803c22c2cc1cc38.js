c874b31392a55db373ad309a12b9e8eddbaab050
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2021-07-05 16:05:11 +0800

    VNI CLAIMS Sprint 13.1 beta Build

--- public/claims/precache-manifest.7a476325f55dbc498803c22c2cc1cc38.js
+++ public/claims/precache-manifest.7a476325f55dbc498803c22c2cc1cc38.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9603fed72e309a973a3e2e3bc70d6b15",
+    "revision": "d02ec185f108acaf7148192063326cd9",
     "url": "/claims/index.html"
   },
   {
-    "revision": "45f4d9cb9692f347b84e",
+    "revision": "4e02ae1a88ec85902f56",
     "url": "/claims/static/css/3.eb3c62c9.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/claims/static/js/2.3d498ac6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "45f4d9cb9692f347b84e",
-    "url": "/claims/static/js/3.32414f46.chunk.js"
+    "revision": "4e02ae1a88ec85902f56",
+    "url": "/claims/static/js/3.bf043b24.chunk.js"
   },
   {
     "revision": "62529cfe8e408bf3e97f",
@@ -40,8 +40,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/claims/static/js/main.5ab617e9.chunk.js"
   },
   {
-    "revision": "5e82425ecb2fb562b8f1",
-    "url": "/claims/static/js/runtime-main.0cf12a33.js"
+    "revision": "8dd32e6e9af933304cc1",
+    "url": "/claims/static/js/runtime-main.ac1bb846.js"
   },
   {
     "revision": "5cb7edfceb233100075dc9a1e12e8da3",
