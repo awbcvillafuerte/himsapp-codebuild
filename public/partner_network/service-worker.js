74515f6ec67453c308e4c9e044d54e40ca415446
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2022-05-23 21:53:44 +0800

    VNI PARTNER NETWORK Sprint 36.2 beta Build

--- public/partner_network/service-worker.js
+++ public/partner_network/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/partner_network/precache-manifest.840a043bb3d224861b50d60b5f4dbc74.js"
+  "/partner_network/precache-manifest.eb1bf46ebf78e05b18bf5842546ad673.js"
 );
 
 self.addEventListener('message', (event) => {
