7957e95ced8d955d8cdd80424811e1d5953fa724
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2022-11-15 19:29:58 +0800

    VNI PARTNER NETWORK Sprint 49.5 beta Build 20221115

--- public/partner_network/precache-manifest.0c8daad09d790a90f210d18b343f9d85.js
+++ public/partner_network/precache-manifest.0c8daad09d790a90f210d18b343f9d85.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1aa9f8b33fad59220dae6924c927fc1b",
+    "revision": "32f34a6eecdbbf633c6623f87f35d127",
     "url": "/partner_network/index.html"
   },
   {
-    "revision": "c48d031ddaefe7e23391",
+    "revision": "e52e8e5b0268865c7630",
     "url": "/partner_network/static/css/6.67c9b8f0.chunk.css"
   },
   {
@@ -80,8 +80,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/partner_network/static/js/3.4e82a50f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c48d031ddaefe7e23391",
-    "url": "/partner_network/static/js/6.8670349c.chunk.js"
+    "revision": "e52e8e5b0268865c7630",
+    "url": "/partner_network/static/js/6.95536981.chunk.js"
   },
   {
     "revision": "c9a8a51c49b52187f3d3",
@@ -104,8 +104,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/partner_network/static/js/main.d589c0e1.chunk.js"
   },
   {
-    "revision": "c0b477048f187c494271",
-    "url": "/partner_network/static/js/runtime-main.0f038fc0.js"
+    "revision": "24d261e334365d6b8707",
+    "url": "/partner_network/static/js/runtime-main.54006431.js"
   },
   {
     "revision": "d95152ab5a160491d28b3fce25bf4ff2",
