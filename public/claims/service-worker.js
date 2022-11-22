843b1106badf121502d4843489dcc6439f180651
Ron Michael Diokno Khu <ron.khu@achievewithoutborders.com>
2022-11-22 21:08:25 +0800

    VNI CLAIMS Sprint 50.2 beta Build

--- public/claims/service-worker.js
+++ public/claims/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/claims/precache-manifest.be10e8e7c86de2bc886ea55a7db0948d.js"
+  "/claims/precache-manifest.c0fdc699d97b7c59ae85a11e14fc95aa.js"
 );
 
 self.addEventListener('message', (event) => {
