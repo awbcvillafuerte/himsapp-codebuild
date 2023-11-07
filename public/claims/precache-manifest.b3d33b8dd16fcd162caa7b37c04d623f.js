68506d586e43b0c1cf4baee1f27e9ac743375029
Crisanto Villafuerte <crisanto.villafuerte@achievewithoutborders.com>
2023-11-07 18:14:59 +0800

    CLAIMS STAGING BUiLD 20230711

--- public/claims/precache-manifest.b3d33b8dd16fcd162caa7b37c04d623f.js
+++ public/claims/precache-manifest.b3d33b8dd16fcd162caa7b37c04d623f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5fd70bc9893ce6b49f116be45081aa5e",
+    "revision": "1cd94a3cd36e0aa96bf6b8b1fb899e59",
     "url": "/claims/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/claims/static/css/2.ce76fce6.chunk.css"
   },
   {
-    "revision": "82ea218729635cdf719f",
+    "revision": "3abbfaf2833e54cc0e63",
     "url": "/claims/static/css/4.b90e0c45.chunk.css"
   },
   {
@@ -32,16 +32,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/claims/static/js/3.a964652b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "82ea218729635cdf719f",
-    "url": "/claims/static/js/4.8c2d3300.chunk.js"
+    "revision": "3abbfaf2833e54cc0e63",
+    "url": "/claims/static/js/4.461d5dec.chunk.js"
   },
   {
     "revision": "18e59de830bf9f455e75",
     "url": "/claims/static/js/main.26fd1749.chunk.js"
   },
   {
-    "revision": "6af95b5fd1c2ef318839",
-    "url": "/claims/static/js/runtime-main.8f87575a.js"
+    "revision": "0212dd17d7ec569877c8",
+    "url": "/claims/static/js/runtime-main.97a1e3c4.js"
   },
   {
     "revision": "5cb7edfceb233100075dc9a1e12e8da3",
