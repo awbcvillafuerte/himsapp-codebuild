fc1e4e481984004f9e2f088dc99b1c38a5e721bd
Crisanto Villafuerte <crisanto.villafuerte@achievewithoutborders.com>
2024-03-07 18:59:52 +0800

    PNM DEV2 BUILD 20240307

--- public/partner_network/precache-manifest.deb9179908eb149a9f9144a0076207b1.js
+++ public/partner_network/precache-manifest.deb9179908eb149a9f9144a0076207b1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b95be768d188d2ca4d30fe0a290cfd85",
+    "revision": "01439c20dd0b753b5c9e08de4cc69eed",
     "url": "/partner_network/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/partner_network/static/css/5.7db51112.chunk.css"
   },
   {
-    "revision": "6e3b01d2716a98a672ff",
+    "revision": "ea8c8ff5851ff4f27e67",
     "url": "/partner_network/static/css/6.dcdbd43e.chunk.css"
   },
   {
@@ -76,12 +76,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/partner_network/static/js/5.a5ac77b7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6e3b01d2716a98a672ff",
-    "url": "/partner_network/static/js/6.6090ea29.chunk.js"
+    "revision": "ea8c8ff5851ff4f27e67",
+    "url": "/partner_network/static/js/6.2c8ba585.chunk.js"
   },
   {
     "revision": "d225879461650baeb743d8134cc09fbb",
-    "url": "/partner_network/static/js/6.6090ea29.chunk.js.LICENSE.txt"
+    "url": "/partner_network/static/js/6.2c8ba585.chunk.js.LICENSE.txt"
   },
   {
     "revision": "ee2089734a5b311db364",
@@ -112,8 +112,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/partner_network/static/js/main.5ab1dd3d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "739cd9b78dcf53377fd9",
-    "url": "/partner_network/static/js/runtime-main.94337371.js"
+    "revision": "d20df9379305b6bf8d08",
+    "url": "/partner_network/static/js/runtime-main.4bc11e5d.js"
   },
   {
     "revision": "d95152ab5a160491d28b3fce25bf4ff2",
